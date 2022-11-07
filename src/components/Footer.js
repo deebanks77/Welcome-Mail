9d2502fdb83446318ee27321b87548983caaa007
Banky <bissbanky@gmail.com>
2022-11-07 18:03:03 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+  FaYoutube,
+  FaFacebook,
+} from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Follow US ON SOCIAL MEDIA</p>
+      <div className="socialIcons">
+        <FaInstagram />
+        <FaTwitter />
+        <FaLinkedin />
+        <FaFacebook />
+        <FaYoutube />
+      </div>
+      <p>12B Admiralty, Lekki. Lagos, Nigeria</p>
+      <p>
+        CopyKart is a trademark and/or registered trademark of Artculture BMC
+      </p>
+      <div>
+        <ul>
+          <li>
+            <a href="">About Us</a>
+          </li>
+          <li>
+            <a href="">Support Desk</a>
+          </li>
+          <li>
+            <a href="">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="">Earning Disclaimer</a>
+          </li>
+          <li>
+            <a href="">Unsubscribe</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Mail.js
@@ -0,0 +1,60 @@
+import React from "react";
+import introverseIcon from "../images/introverseLogo.png";
+import flowerIcon from "../images/flower.png";
+import arrow from "../images/arrow.png";
+import Footer from "./Footer";
+
+function Mail() {
+  return (
+    <>
+      <div className="img">
+        <img src={introverseIcon} alt="introverse" className="logo" />
+      </div>
+      <div className="thankYou">
+        <img src={flowerIcon} alt="flower" className="flower" />
+        <p>
+          Thanks for choosing IntroVerse. We are excited to have you onboard
+          with us.
+        </p>
+      </div>
+      <div className="message">
+        <p>Hey [First Name],</p>
+        <p>Welcome to Introverse! </p>
+        <p>We are excited to have you onboard. Are you excited? </p>
+        <p>
+          Your log in details should be appearing in your inbox before too long!
+        </p>
+        <p>
+          Remember, you are not to share any information via social media or any
+          other outlet. If they do, you will be banned from the test, your
+          account will be blocked, and may face legal charges or penalties.{" "}
+        </p>
+        <p>
+          In the meantime, we thought you might like to get to know Introverse a
+          little better-so we put together a quick video demo of what it is all
+          about.
+        </p>
+        <p className="link">
+          https://www.loom.com/share/55e9ae418c3543868c3dd80e507ae483
+        </p>
+        <p>
+          Of course, that's just a quick overview of what the software is all
+          about and pretty soon, you'll be taking Introverse for a spin
+          yourself!
+        </p>
+        <p>Looking forward to get your honest reviews/feedback...</p>
+        <p>Cheers,</p>
+      </div>
+      <div className="Ps">
+        <p>PS: Want to get a head start?</p>
+        <button>
+          Create your first logo/intro video here
+          <img src={arrow} alt="arrow" className="arrow" />
+        </button>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Mail;
